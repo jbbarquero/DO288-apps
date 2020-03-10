ebc473b53089a5d5adac0efb97312f749b687555
Student User <student@workstation.lab.example.com>
2020-03-10 15:05:37 +0000

    fixed new app script with the appropriate template yaml

--- templates/oc-new-app.sh
+++ templates/oc-new-app.sh
@@ -2,7 +2,7 @@
 
 source /usr/local/etc/ocp4.config
 
-oc new-app --name todo --file ~/todo-template.yaml \
+oc new-app --name todo --file todo-template.yaml \
     -p APP_GIT_URL=https://github.com/${RHT_OCP4_GITHUB_USER}/DO288-apps \
     -p NPM_PROXY=http://${RHT_OCP4_NEXUS_SERVER}/repository/nodejs \
     -p PASSWORD=mypass \
